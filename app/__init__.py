2142f1f50f6d718a4c06fd2c96e24f44dfb5c441
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-09-26 00:11:00 +0200

    Update users' form

--- app/__init__.py
+++ app/__init__.py
@@ -1,45 +1,51 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from app.config import Config
-from flask_migrate import Migrate
-from app.config import config_options
-from flask_mail import Mail
-from app import error
+#from flask_mail import Mail
+#from flask_uploads import UploadSet,configure_uploads,IMAGES
 
-
-login_manager = LoginManager()
-login_manager.login_view = 'users.login'
-login_manager.login_message_category = 'strong'
-
-
-app = Flask(__name__)
-mail = Mail()
+bootstrap = Bootstrap()
 db = SQLAlchemy()
-bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
+#mail = Mail()
+#photos = UploadSet('photos',IMAGES)
 
 
 def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # configure UploadsSet
+    #configure_uploads(app,photos)
     
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
-    
-    app.config['SECRET_KEY'] = 'banana'
-    print(app.config['SQLALCHEMY_DATABASE_URI'])
-    app.config['DEBUG']=True
+    config_options[config_name].init_app(app)
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
     db.init_app(app)
-    bcrypt.init_app(app)
     login_manager.init_app(app)
-    mail.init_app(app)
-    
-    from app.users.views import users 
-    from app.posts.views import posts
-    from app.main.views import main 
-    
-    app.register_blueprint(users)
-    app.register_blueprint(posts)
-    app.register_blueprint(main)
+    #mail.init_app(app)
+
+
+    # Registering the blueprints
+    from .main import main as main_blueprint
+    from .auth import auth as auth_blueprint
+
+    app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    # configure uploads
+    #configure_uploads(app,photos)
+
+    # setting config
+    #from .requests import configure_request
+    #configure_request(app)
 
     return app
--- app/email.py
+++ app/email.py
@@ -1,12 +1,10 @@
-from flask_mail import Message
-from flask import render_template
-from . import mail
+# #from flask_mail import Message
+# from flask import render_template
+# #from . import mail
 
-subject_pref = 'Pitch'
-sender_email = 'semwagacarine@gmail.com'
-
-def mail_message(subject, template, to, **kwargs):
-    email = Message(subject_pref+subject, sender=sender_email, recipients=[to])
-    email.body= render_template(template + ".txt",**kwargs)
-    email.html = render_template(template + ".html",**kwargs)
-    mail.send(email)
+# def mail_message(subject,template,to,**kwargs):
+#     sender_email = wambukeren@gmail.com
+#     email = Message(subject, sender=sender_email,recipients=[to])
+#     email_body = render_template(template+".txt",**kwargs)
+#     email.html = render_template(template+".html",**kwargs)
+#     mail.send(email)
