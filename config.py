f3b61d52096168da3bc1fbec5076fb62c579b02a
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-03 05:27:22 +0200

    Resolve - bg(04)

--- config.py
+++ config.py
@@ -1,32 +1,31 @@
 import os
 
+
 class Config:
-
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:wecode@localhost/pitch'
-    UPLOADED_PHOTOS_DEST ='app/static/profile_pics'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    #  email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:wecode@localhost/pitchesdb'
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
-    @staticmethod
-    def init_app(app):
-        pass
-
-
-class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:wecode@localhost/pitchesdb'
     DEBUG = True
 
-config_options = {
-'development':DevConfig,
-'production':ProdConfig
+class ProdConfig(Config):
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    pass
 
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:wecode@localhost/pitchesdb_test'
+
+
+configurations = {
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test':TestConfig
 }
